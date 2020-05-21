f58453dac8a2d49358964c9abc61af8b4dc185f5
Ondřej Tománek <andrewtomanek@gmail.com>
2020-05-21 23:25:00 +0200

    moved fetching repo to rematch reducer

--- src/pages/RepoDetail.js
+++ src/pages/RepoDetail.js
@@ -1,45 +1,33 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import { connect } from "react-redux";
 import RepoCard from "../components/RepoCard";
-import { PageLayout, BasicText } from "../styles/elements";
+import { PageLayout } from "../styles/elements";
 
 const RepoDetail = (props) => {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [repoData, setRepoData] = useState([]);
   let { repoName } = useParams();
 
   useEffect(() => {
     if (props.user.user) {
-      props.user.user && getRepo();
+      props.user.user && props.loadUserRepo([props.user.user.login, repoName]);
     } else {
       props.history.push("/");
     }
   }, []);
 
-  const getRepo = async () => {
-    const repoUrl = `https://api.github.com/repos/${props.user.user.login}/${repoName}`;
-
-    try {
-      const repoDataResponse = await axios.get(repoUrl);
-      const repoDetail = repoDataResponse.data;
-      setRepoData(repoDetail);
-    } catch (error) {
-      setErrorMessage(error.response.statusText);
-    }
-  };
-
   return (
     <PageLayout>
-      {errorMessage && <BasicText>{errorMessage}</BasicText>}
-      <RepoCard repo={repoData} />
+      {props.user.repo && <RepoCard repo={props.user.repo} />}
     </PageLayout>
   );
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  loadUserRepo: (repoIdArray) => dispatch.user.loadUserRepo(repoIdArray),
+});
 
 const mapStateToProps = (state) => ({
   user: state.user,
 });
 
-export default connect(mapStateToProps)(RepoDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(RepoDetail);
--- src/state/models.js
+++ src/state/models.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const baseUrl = "https://api.github.com/users";
+const repoUrl = `https://api.github.com/repos`;
 
 export const user = {
   state: {
@@ -12,6 +13,12 @@ export const user = {
         user: responseData,
       };
     },
+    loadRepo(state, responseData) {
+      return {
+        ...state,
+        repo: responseData,
+      };
+    },
   },
   effects: (dispatch) => ({
     async loadUserData(name) {
@@ -19,7 +26,17 @@ export const user = {
         const response = await axios.get(`${baseUrl}/${name}`);
         dispatch.user.loadData(response.data);
       } catch (error) {
-       console.log(error)
+        console.log(error);
+      }
+    },
+    async loadUserRepo(repoIdArray) {
+      try {
+        const response = await axios.get(
+          `${repoUrl}/${repoIdArray[0]}/${repoIdArray[1]}`
+        );
+        dispatch.user.loadRepo(response.data);
+      } catch (error) {
+        console.log(error);
       }
     },
   }),
